61d426dc66048215939409e16c01f42e09cf863d
LAPTOP-87U0IGP7\Rohit <rohitdotacustom@gmail.com>
2016-09-01 16:10:52 -0400

    use execute order from table instead of movetoposition

--- game/dota_addons/dire-egg-timebomb/scripts/vscripts/moveorders.lua
+++ game/dota_addons/dire-egg-timebomb/scripts/vscripts/moveorders.lua
@@ -1,18 +1,21 @@
 function ExecuteMoveOrders(unit)
 	for _,d in pairs(unit.destinations) do
+
 		local xPos = d["0"]
 		local yPos = d["1"]
 		local zPos = d["2"]
-		print(unit.timerName .. " is moving to (" .. xPos .. "," .. yPos .. "," .. zPos .. ")")
+		print("[MoveOrders] " .. unit.timerName .. " is moving to (" .. xPos .. "," .. yPos .. "," .. zPos .. ")")
+
+
 		local dest = Vector(xPos, yPos, zPos)
-		order = {
+		local order = {
 			UnitIndex = unit:GetEntityIndex(),
 			OrderType = DOTA_UNIT_ORDER_MOVE_TO_POSITION,
 			Position = dest,
 			Queue = 1
 		}
-		-- ExecuteOrderFromTable(order)
-		unit:MoveToPosition(dest)
+		ExecuteOrderFromTable(order)
+		--unit:MoveToPosition(dest)
 	end
 	unit.destinations = {}
 end
@@ -22,5 +25,5 @@ function RegisterMoveOrder(cursor_position)
 	-- TODO: use GridNav:CanFindPath to check if the move is valid
 	local activeUnit = TurnTimers.activeUnit
 	table.insert(activeUnit.destinations, cursor_position)
-	print(activeUnit.timerName .. "'s move order to " .. "(" .. cursor_position["0"] .. "," .. cursor_position["1"] .. "," .. cursor_position["2"] .. ") registered")
+	print("[MoveOrders] " .. activeUnit.timerName .. "'s move order to " .. "(" .. cursor_position["0"] .. "," .. cursor_position["1"] .. "," .. cursor_position["2"] .. ") registered")
 end
